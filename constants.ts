3de748246f8b3d51105d121e70d2fb642f8cd51e
Phạm Gia Huy <88099196+hml69@users.noreply.github.com>
2026-02-20 17:00:47 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -6,7 +6,6 @@ export const SKILLS: Skill[] = [
   { name: 'Laravel', category: 'Backend', level: 36 },
   { name: 'React', category: 'Frontend', level: 36 },
   { name: 'Next.js', category: 'Frontend', level: 36 },
-  { name: 'Vite', category: 'Frontend', level: 36 },
   { name: 'Nginx', category: 'DevOps', level: 36 },
   { name: 'Apache', category: 'DevOps', level: 36 },
 ];
